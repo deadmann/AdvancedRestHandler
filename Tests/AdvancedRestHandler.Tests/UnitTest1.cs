000510350fe037e79b28f21157966bab5088cf64
mimosh.pisholack <mimosh.pisholack@gmail.com>
2020-12-15 19:20:45 +0330

    Provide UnitTest Solution, Still no work is done

---
+++ Tests/AdvancedRestHandler.Tests/UnitTest1.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using NUnit.Framework;
+
+namespace AdvancedRestHandler.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //Assert.Pass();
+
+            // ARRANGE
+            var handlerMock = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handlerMock
+                .Protected()
+                // Setup the PROTECTED method to mock
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                // prepare the expected response of the mocked http call
+                .ReturnsAsync(new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("[{'id':1,'value':'1'}]"),
+                })
+                .Verifiable();
+
+            // use real http client with mocked handler here
+            var httpClient = new HttpClient(handlerMock.Object)
+            {
+                BaseAddress = new Uri("http://test.com/"),
+            };
+
+            var subjectUnderTest = new MyTestClass(httpClient);
+
+            // ACT
+            var result = await subjectUnderTest
+                .GetSomethingRemoteAsync("api/test/whatever");
+
+            // ASSERT
+            result.Should().NotBeNull(); // this is fluent assertions here...
+            result.Id.Should().Be(1);
+
+            // also check the 'http' call was like we expected it
+            var expectedUri = new Uri("http://test.com/api/test/whatever");
+
+            handlerMock.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1), // we expected a single external request
+                ItExpr.Is<HttpRequestMessage>(req =>
+                        req.Method == HttpMethod.Get  // we expected a GET request
+                        && req.RequestUri == expectedUri // to this uri
+                ),
+                ItExpr.IsAny<CancellationToken>()
+            );
+        }
+    }
+}
