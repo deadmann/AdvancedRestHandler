f7084cbb5a82d68ef82741db96e0c3bfe3689674
Hassan Faghihi <hassan.faghihi@outlook.com>
2024-04-21 11:18:08 +0330

    #42: Add support for IHttpClientFactory and HttpClient
     - Adding dependency to `Microsoft.Extensions.Http@2.1.1`

--- AdvancedRestHandler/RestHandlerInitializerOptions.cs
+++ AdvancedRestHandler/RestHandlerInitializerOptions.cs
@@ -9,12 +9,17 @@ namespace Arh
     [SuppressMessage("ReSharper", "RedundantDefaultMemberInitializer")]
     [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
     [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
     public class RestHandlerInitializerOptions
     {
         /// <summary>
         /// In some web framework, existence or not existence of slash '/' can cause to invoke an incorrect url; 
         /// This feature can be turned of, but requires a manually handling of URL's slash, at the end of base url or begin of partial urls
         /// </summary>
+        /// <remarks>
+        /// Supports only when creating HttpClient internal.<br/>
+        /// It is not supported when providing `HttpClient`, or `IHttpClientFactory` 
+        /// </remarks>
         public bool FixEndOfUrl { get; set; }
 
         /// <summary>
